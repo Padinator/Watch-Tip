233ecad0591ed9f804d132c5cde9f10b475d7f8a
Tom Lauth <66142861+lauthtom@users.noreply.github.com>
2024-12-14 13:15:50 +0100

    Test class for the parallelizer file

---
+++ tests/test_helper/test_parallelizer.py
@@ -0,0 +1,11 @@
+import json
+import unittest
+
+from helper.parallelizer import parallelize_task_and_return_results
+
+
+class TestApiRequester(unittest.TestCase):
+    
+    # TODO: This class need still some tests
+    def test_parallelize_task_and_return_results(self):
+        pass
