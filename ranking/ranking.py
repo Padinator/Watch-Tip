3ea1303110563ad68ec66ac3a10934cccae26c14
Tom Lauth <66142861+lauthtom@users.noreply.github.com>
2024-12-21 17:02:16 +0100

    Restructuring the code and wrote documentation

--- ranking/ranking.py
+++ ranking/ranking.py
@@ -7,6 +7,7 @@ import sys
 import google.generativeai as genai
 
 from pathlib import Path
+from typing import Any, Dict
 
 # ---------- Import own python modules ----------
 project_dir = Path(__file__).parents[1]
@@ -92,50 +93,111 @@ filtered_reviews = get_reviews_by_movie_ids(all_users, movies)
 #     print(f"Rezension: {review}")
 #     print(f"Analyse: {analyze_user_review(review)}")
 
-# Gemini variant and their limits
-#   - 15 requests per minute
-#   - 1 million tokens per minute
-#   - 1500 requests per day
-genai_api_key = os.environ["genai"]
-genai.configure(api_key=genai_api_key)
-system_instruction = """
-You are a text analysis expert. Your task is to analyze user reviews of movies and evaluate their sentiment. For each review, follow these steps:
-1. Identify the overall sentiment of the review as positive, neutral, or negative.
-2. Assign a sentiment score on a scale from -1 to 1:
-   - Positive sentiment: 0.1 to 1.0
-   - Neutral sentiment: -0.1 to 0.1
-   - Negative sentiment: -1.0 to -0.1
-3. Highlight key reasons or phrases that influenced the sentiment classification.
 
-Always return your response in the following JSON format:
-{
-  "sentiment": "positive/neutral/negative",
-  "score": float (between -1 and 1),
-  "highlights": ["key phrase 1", "key phrase 2", ...]
-}
-"""
+def set_up_llm(model_name: str, system_instruction: str) -> genai.GenerativeModel:
+    # TODO: Currently only working with the google 'gemini' model
+    # Setup and test this also with the openai 'gpt' model
+    """
+    Set up a generative large language model with the specified model name and system instruction.
 
-model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
-    system_instruction=system_instruction,
-)
+    Parameters
+    ----------
+    model_name : str
+        The name of the large language model to be used.
+    system_instruction : str
+        The system instruction to configure the large language model.
 
-movies_with_their_responses = {}
+    Returns
+    -------
+    genai.GenerativeModel
+        An instance of the generative model configured with the specified parameters.
+    """
+    return genai.GenerativeModel(
+        model_name=model_name,
+        system_instruction=system_instruction,
+    )
 
-for movie, review in filtered_reviews.items():
-    response = model.generate_content(f"Analyze the following review: {review}")
 
-    cleaned_response = re.search(r"\{.*?\}", response.text, re.DOTALL)
-    if cleaned_response:
-        try:
-            response_data = json.loads(cleaned_response.group())
-            movies_with_their_responses[movie] = response_data
-        except json.JSONDecodeError as e:
-            print(f"Error parsing cleaned JSON for {movie}: {e}")
-    else:
-        print(f"No valid JSON found in response for {movie}")
-        movies_with_their_responses[movie] = None
+def generate_ai_response(filtered_reviews: Dict[str, str], model: genai.GenerativeModel) -> Dict[str, Dict[str, Any]]:
+    """
+    Generate AI responses for a set of best matching movies on the user preferences
 
-for movie, response in movies_with_their_responses.items():
-    print(f"Movie: {movie}\nResponse: {response}")
-    print("-" * 100)
+    Parameters
+    ----------
+    filtered_reviews : dict of str
+        A dictionary where the keys are movie titles and the values are their corresponding reviews.
+
+    Returns
+    -------
+    dict of dict
+        A dictionary where the keys are movie titles and the values are dictionaries containing the AI-generated responses.
+        If the response cannot be parsed as JSON, the value will be None.
+
+    Notes
+    -----
+    This function uses a model to generate content based on the provided reviews. It expects the model's response to contain
+    a JSON object. If the JSON object cannot be parsed, an error message will be printed and the corresponding movie's value
+    in the returned dictionary will be None.
+    """
+    movies_with_their_responses = {}
+    for movie, review in filtered_reviews.items():
+        response = model.generate_content(f"Analyze the following review: {review}")
+
+        cleaned_response = re.search(r"\{.*?\}", response.text, re.DOTALL)
+        if cleaned_response:
+            try:
+                response_data = json.loads(cleaned_response.group())
+                movies_with_their_responses[movie] = response_data
+            except json.JSONDecodeError as e:
+                print(f"Error parsing cleaned JSON for {movie}: {e}")
+        else:
+            print(f"No valid JSON found in response for {movie}")
+            movies_with_their_responses[movie] = None
+
+    return movies_with_their_responses
+
+
+def print_in_clean_format(movies_with_their_responses: Dict[str, Dict[str, Any]]) -> None:
+    """
+    Prints the movies and their responses in a clean format.
+
+    Parameters
+    ----------
+    movies_with_their_responses : dict of dict
+        A dictionary where the keys are movie titles (str) and the values are dictionaries containing responses (dict).
+
+    Returns
+    -------
+    None
+    """
+    for movie, response in movies_with_their_responses.items():
+        print(f"Movie: {movie}\nResponse: {response}")
+        print("-" * 100)
+
+
+if __name__ == "__main__":
+
+    genai.configure(api_key=os.environ["genai"])
+
+    system_instruction = """
+        You are a text analysis expert. Your task is to analyze user reviews of movies and evaluate their sentiment. For each review, follow these steps:
+        1. Identify the overall sentiment of the review as positive, neutral, or negative.
+        2. Assign a sentiment score on a scale from -1 to 1:
+        - Positive sentiment: 0.1 to 1.0
+        - Neutral sentiment: -0.1 to 0.1
+        - Negative sentiment: -1.0 to -0.1
+        3. Highlight key reasons or phrases that influenced the sentiment classification.
+
+        Always return your response in the following JSON format:
+        {
+        "sentiment": "positive/neutral/negative",
+        "score": float (between -1 and 1),
+        "highlights": ["key phrase 1", "key phrase 2", ...]
+        }
+    """
+
+    llm_model = set_up_llm("gemini-1.5-flash", system_instruction)
+
+    movies_with_their_responses = generate_ai_response(filtered_reviews, llm_model)
+
+    print_in_clean_format(movies_with_their_responses)
